d6d9b0294f6da19d6f67e70afb1c94fb64a58c94
SignorinoY <meetziyang@gmail.com>
2021-04-05 14:32:40 +0800

    :zap: add example_input_array

--- src/model/classfier.py
+++ src/model/classfier.py
@@ -24,6 +24,7 @@ class FashionMNISTClassfier(pl.LightningModule):
     def __init__(self, learning_rate=1e-3):
         super().__init__()
         self.save_hyperparameters()
+        self.example_input_array = torch.zeros(1, 1, 28, 28)
         self.conv1 = nn.Conv2d(1, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
